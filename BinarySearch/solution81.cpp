db440b754b27a2fac666a345102ea8d266132790
Nid_Contributor <nidhish21works@gmail.com>
2025-09-03 15:40:31 +0530

    Solved problem 81 on leetcode

---
+++ BinarySearch/solution81.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return true;
+
+            if (nums[mid] == nums[right]) {
+                right--;
+            }
+            else if (nums[mid] < nums[right]) {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            else {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {2,5,6,0,0,1,2};
+    int target = 0;
+    cout << s.search(nums, target) << endl;
+}
