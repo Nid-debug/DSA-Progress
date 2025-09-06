2f5a6668811bd37bf5d487a217762a4150f18d1c
Nid_Contributor <nidhish21works@gmail.com>
2025-09-07 02:53:14 +0530

    Solved problem 189 on leetcode

---
+++ arrayProblems/solution189.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        auto reverseArr = [&](int l, int r) {
+            while (l < r) {
+                swap(nums[l], nums[r]);
+                l++, r--;
+            }
+        };
+        reverseArr(0, n-1);     
+        reverseArr(0, k-1);     
+        reverseArr(k, n-1);     
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+    s.rotate(nums, k);
+    cout << "Rotated Array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+}
