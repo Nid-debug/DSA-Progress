50306345fbf6d2bf8348992fbbd85f5c5692a05f
Nid_Contributor <nidhish21works@gmail.com>
2025-11-02 02:05:15 +0530

    Solved problem 61 : Rotate List on leetcode

---
+++ LinkedList/61_RotateList.cpp
@@ -0,0 +1,33 @@
+// LeetCode 61. Rotate List
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+        
+        ListNode* tail = head;
+        int length = 1;
+        while (tail->next) {
+            tail = tail->next;
+            length++;
+        }
+        
+        k = k % length;
+        if (k == 0) {
+            return head;
+        }
+        
+        ListNode* newTail = head;
+        for (int i = 0; i < length - k - 1; i++) {
+            newTail = newTail->next;
+        }
+        
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;  
+        tail->next = head;        
+        
+        return newHead;
+    }
+};
