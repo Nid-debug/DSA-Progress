c8a3364a3207dd588c998222aa010ec6aea17cc4
Nid_Contributor <nidhish21works@gmail.com>
2025-11-20 16:04:55 +0530

    Solved problem 355: Design Twitter on leetcode

--- Heaps/355_Design_Twitter.cpp
+++ Heaps/355_Design_Twitter.cpp
@@ -1,12 +1,19 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 using namespace std;
 
 class Twitter {
 private:
+    // Store tweets: userId -> vector of (timestamp, tweetId)
     unordered_map<int, vector<pair<int, int>>> tweets;
     
+    // Store following relationships: userId -> set of userIds they follow
     unordered_map<int, unordered_set<int>> following;
-
+    
+    // Global timestamp to track tweet order
     int timestamp;
     
 public:
@@ -15,18 +22,23 @@ public:
     }
     
     void postTweet(int userId, int tweetId) {
+        // Add tweet with current timestamp
         tweets[userId].push_back({timestamp++, tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
+        // Max heap to store (timestamp, tweetId)
+        // Priority queue is max heap by default
         priority_queue<pair<int, int>> maxHeap;
         
+        // Add user's own tweets to heap
         if (tweets.find(userId) != tweets.end()) {
             for (auto& tweet : tweets[userId]) {
                 maxHeap.push(tweet);
             }
         }
         
+        // Add tweets from all users that userId follows
         if (following.find(userId) != following.end()) {
             for (int followeeId : following[userId]) {
                 if (tweets.find(followeeId) != tweets.end()) {
@@ -37,6 +49,7 @@ public:
             }
         }
         
+        // Get top 10 most recent tweets
         vector<int> newsFeed;
         int count = 0;
         while (!maxHeap.empty() && count < 10) {
@@ -49,6 +62,7 @@ public:
     }
     
     void follow(int followerId, int followeeId) {
+        // User cannot follow themselves
         if (followerId != followeeId) {
             following[followerId].insert(followeeId);
         }
