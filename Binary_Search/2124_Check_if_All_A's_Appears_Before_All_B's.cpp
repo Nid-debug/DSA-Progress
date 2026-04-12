d68ff2869296519f76947e5e77d8787a748a1eff
Nid_Contributor <nidhish21works@gmail.com>
2026-04-12 20:18:57 +0530

    Solved problem 2124: Check if All A's Appears Before All B's on leetcode

---
+++ Binary_Search/2124_Check_if_All_A's_Appears_Before_All_B's.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool checkString(string s) {
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i-1] == 'b' && s[i] == 'a') {
+                return false; 
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution p;
+    string s = "aabbb";
+    cout << p.checkString(s) << endl;
+}
