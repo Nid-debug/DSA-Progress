2127f25a238747e83f57c0dba17588a92d076411
Nid_Contributor <nidhish21works@gmail.com>
2026-04-12 20:29:26 +0530

    Solved problem 875: Koko Eating Bananas on leetcode

--- Binary_Search/875_Koko_Eating_Bananas.cpp
+++ Binary_Search/875_Koko_Eating_Bananas.cpp
@@ -30,10 +30,3 @@ public:
         return ans;
     }
 };
-
-int main() {
-    Solution s;
-    vector<int> piles = {30,11,23,4,20};
-    int h = 6;
-    cout << s.minEatingSpeed(piles, h) << endl;
-}
