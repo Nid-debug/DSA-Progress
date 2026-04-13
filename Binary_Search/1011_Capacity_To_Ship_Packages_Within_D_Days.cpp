e321c8b127db2bc34c9f8c0ab380903e08f2039d
Nid_Contributor <nidhish21works@gmail.com>
2026-04-13 13:51:56 +0530

    Solved problem 1011: Capacity To Ship Packages Within D Days on leetcode

---
+++ Binary_Search/1011_Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+========================================
+    Binary Search on Answer
+========================================
+Problem: Capacity to Ship Packages Within D Days
+Time Complexity: O(N * log(sum(weights)))
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    bool possible(long long mid, vector<int>& weights, int days) {
+        int daysNeeded = 1;
+        long long sum = 0;
+
+        for (int i = 0; i < weights.size(); i++) {
+            if (sum + weights[i] > mid) {
+                daysNeeded++;
+                sum = 0;
+            }
+            sum += weights[i];
+        }
+        return daysNeeded <= days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        long long low = *max_element(weights.begin(), weights.end());
+        long long high = 0;
+
+        for (int w : weights) {
+            high += w;
+        }
+
+        while (low < high) {
+            long long mid = low + (high - low) / 2;
+
+            if (possible(mid, weights, days)) {
+                high = mid;        // try smaller capacity
+            } 
+            else {
+                low = mid + 1;     // need more capacity
+            }
+        }
+        return low;
+    }
+};
